b522b84e335639218bac92734dbdab9bd8dc0250
Joseph Collard <jcollard@users.noreply.github.com>
2022-08-07 06:01:04 -0700

    chore: Add metafiles that changed.

--- Character Customizer/Assets/GameController.cs
+++ Character Customizer/Assets/GameController.cs
@@ -1,37 +1,42 @@
 using System.Collections;
 using System.Collections.Generic;
+using Firebase.Database;
 using UnityEngine;
-using FirebaseWebGL.Scripts.FirebaseBridge;
+
 public class GameController : MonoBehaviour
 {
     // Start is called before the first frame update
     async void Start()
     {
-        // DatabaseReference reference = FirebaseDatabase.DefaultInstance.GetReference("test");
-        // System.Threading.Tasks.Task<DataSnapshot> x = reference.GetValueAsync();
-        // DataSnapshot y = await x;
-        // Debug.Log(y.GetValue(false));
+        DatabaseReference reference = FirebaseDatabase.DefaultInstance.GetReference("test");
+        System.Threading.Tasks.Task<DataSnapshot> x = reference.GetValueAsync();
+        DataSnapshot y = await x;
+        Debug.Log(y.GetValue(false));
 
     }
 
     public void Authenticate()
     {
-        FirebaseAuth.SignInWithGoogle(gameObject.name, "DisplayInfo", "DisplayErrorObject");
-    }
+        Firebase.Auth.FirebaseAuth auth = Firebase.Auth.FirebaseAuth.DefaultInstance;
+        auth.SignInWithEmailAndPasswordAsync("test@user.com", "test@user.comspa$$word1$eZ").ContinueWith(task => {
+        if (task.IsCanceled) {
+            Debug.LogError("SignInWithEmailAndPasswordAsync was canceled.");
+            return;
+        }
+        if (task.IsFaulted) {
+            Debug.LogError("SignInWithEmailAndPasswordAsync encountered an error: " + task.Exception);
+            return;
+        }
 
-    public void DisplayInfo(string info)
-    {
-        Debug.Log(info);
-    }
-
-    public void DisplayErrorObject(string error)
-    {
-        Debug.Log(error);
+        Firebase.Auth.FirebaseUser newUser = task.Result;
+        Debug.LogFormat("User signed in successfully: {0} ({1})",
+            newUser.DisplayName, newUser.UserId);
+        });
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        
     }
 }
